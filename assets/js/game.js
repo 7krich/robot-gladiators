a43b935e37acd48f11819f50fa11e6b8c9d31148
Kyle Richnafsky <7krich@gmail.com>
2022-02-19 13:03:56 -0500

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,7 +68,7 @@ var fight = function (enemyName) {
       }
     };
 
-    //function to start a new game
+    // function to start a new game
     var startGame = function() {
       //reset player stats
       playerHealth = 100;
@@ -90,8 +90,20 @@ var fight = function (enemyName) {
       
           // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
           fight(pickedEnemyName);
+
+          // if player is still alive and we're not at the last enemy in the array
+          if (playerHealth > 0 && i < enemyNames.length - 1) {
+            // ask if player wants to use the store before next round
+            var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+            // if yes, take them to the store() function
+            if (storeConfirm) {
+            shop();
+            }
+          }
+
         } else {
-            window.alert("You have lost your robot in battle! Game Over!");
+            window.alert("You  have lost your robot in battle! Game Over!");
             break;
         }
       }
@@ -103,7 +115,7 @@ var fight = function (enemyName) {
     var endGame = function() {
       // if plyer is still alive, player wins!
       if (playerHealth > 0) {
-        window.alert("Great jobe, you've survived the game! You now have a score of " + playerMoney + ".");
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
       } else {
         window.alert("You've lost your robot in battle.");
       }
@@ -119,6 +131,54 @@ var fight = function (enemyName) {
       }
     };
 
+    var shop = function() {
+      //ask player what they'd like to do
+      var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+      );
+
+      // use switch to carry out action
+      switch (shopOptionPrompt) {
+        case "REFILL": // new case
+        case "refill":
+          if (playerMoney >= 7) {
+            window.alert("Refilling player's health by 20 for 7 dollars.");
+
+          // increase health and decrease money
+          playerHealth = playerHealth + 20;
+          playerMoney = playerMoney - 7;
+          } else {
+            window.alert("You don't have enough money!");
+          }
+
+          break;
+        case "UPGRADE": // new case
+        case "upgrade":
+          if (playerMoney >= 7) {
+          window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+          // increase attack an decrease money
+          playerAttack = playerAttack + 6;
+          playerMoney = playerMoney - 7;
+          } else {
+            window.alert("You don't have enough money!");
+          }
+
+          break;
+        case"lEAVE": // new case
+        case "leave":
+          window.alert("Leaving the store.");
+
+          // do nothing, so funciton will end
+          break;
+        default:
+          window.alert("You did not pick a valid option. Try again.");
+
+          // call shop() again to force player ot pick a valid option
+          shop();
+          break;
+      }
+    };
 
 //start the game when the page loads
 startGame();
